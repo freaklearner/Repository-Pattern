abaf09129152175f2f8b2746991839cd936d62ba
freaklearner <shub.shubhamthakur@gmail.com>
2019-11-17 17:04:35 +0530

    ProductDbContext register in startup.cs file

--- RepositoryPattern/Startup.cs
+++ RepositoryPattern/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using RepositoryPattern.Model;
 
 namespace RepositoryPattern
 {
@@ -23,13 +25,19 @@ namespace RepositoryPattern
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public static void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(x=>x.EnableEndpointRouting=false).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<ProductDbContext>(config =>
+            {
+                config.UseMySQL("server=localhost;database=test;username=root;password=admin@123");
+                
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public static void Configure(IApplicationBuilder app, IHostingEnvironment env, ProductDbContext productDbContext)
         {
             if (env.IsDevelopment())
             {
@@ -42,6 +50,7 @@ namespace RepositoryPattern
 
             app.UseHttpsRedirection();
             app.UseMvc();
+            productDbContext.Database.EnsureCreated();
         }
     }
 }
