a18e59dc41779ac1b79f8c3427d415fbdab003a6
freaklearner <shub.shubhamthakur@gmail.com>
2019-11-17 18:27:22 +0530

    IActionFilter added for validation of Product object

---
+++ RepositoryPattern/Filters/ModelValidationFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RepositoryPattern.Filters
+{
+    public class ModelValidationFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState);
+            }
+        }
+    }
+}
