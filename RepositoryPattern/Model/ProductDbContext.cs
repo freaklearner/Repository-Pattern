ec9602842575cea01fad4eeb3c03361b77498bdd
freaklearner <shub.shubhamthakur@gmail.com>
2019-11-17 16:55:59 +0530

    Product and DbContext class created

---
+++ RepositoryPattern/Model/ProductDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern.Model
+{
+    public class ProductDbContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>(entery =>
+            {
+                entery.HasKey(x => x.Id);
+                entery.Property(x => x.Name).IsRequired();
+                entery.Property(x => x.Price).IsRequired();
+            });
+        }
+
+
+
+    }
+}
