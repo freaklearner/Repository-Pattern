6e6a8fc1350df427c8a50318d8f39233b617eb0c
freaklearner <shub.shubhamthakur@gmail.com>
2019-11-17 17:17:21 +0530

    IProduct interface created

---
+++ RepositoryPattern/Interfaces/IProduct.cs
@@ -0,0 +1,21 @@
+﻿using RepositoryPattern.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern.Interfaces
+{
+    interface IProduct
+    {
+        IEnumerable<Product> Get();
+
+        Product Get(int id);
+
+        bool Post(Product product);
+
+        bool Put(Product product);
+
+        bool Delete(int id);
+    }
+}
